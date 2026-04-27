9f01af357a29408babf89910c3173cd983db8b46
CamdenMann <147839064+CamdenMann@users.noreply.github.com>
2026-04-27 10:19:03 -0400

    Add files via upload

---
+++ site-data.js
@@ -0,0 +1,98 @@
+window.CASH_STASH_DATA = {
+  "brandName": "Cash Stash",
+  "domain": "cashstash.com",
+  "primaryCTA": "Partner With Cash Stash",
+  "stats": [
+    {
+      "number": "100K+",
+      "label": "Followers across the network"
+    },
+    {
+      "number": "1M+",
+      "label": "Average reach across major stash campaigns"
+    },
+    {
+      "number": "8",
+      "label": "Active city accounts"
+    },
+    {
+      "number": "Multi-city",
+      "label": "Sponsor campaign capability"
+    }
+  ],
+  "nationalForm": "https://formspree.io/f/REPLACE_MAIN_FORM",
+  "cities": [
+    {
+      "id": "raleigh",
+      "name": "Raleigh",
+      "state": "NC",
+      "handle": "@cashstashraleigh",
+      "instagram": "https://www.instagram.com/cashstashraleigh",
+      "form": "https://formspree.io/f/REPLACE_RALEIGH_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "durham",
+      "name": "Durham",
+      "state": "NC",
+      "handle": "@cashstashdurham",
+      "instagram": "https://www.instagram.com/cashstashdurham",
+      "form": "https://formspree.io/f/REPLACE_DURHAM_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "wilmington",
+      "name": "Wilmington",
+      "state": "NC",
+      "handle": "@cashstashwilmington",
+      "instagram": "https://www.instagram.com/cashstashwilmington",
+      "form": "https://formspree.io/f/REPLACE_WILMINGTON_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "grand-rapids",
+      "name": "Grand Rapids",
+      "state": "MI",
+      "handle": "@cashstashgr",
+      "instagram": "https://www.instagram.com/cashstashgr",
+      "form": "https://formspree.io/f/REPLACE_GRAND_RAPIDS_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "dfw",
+      "name": "Fort Worth / Dallas",
+      "state": "TX",
+      "handle": "@cashstashdfw",
+      "instagram": "https://www.instagram.com/cashstashdfw",
+      "form": "https://formspree.io/f/REPLACE_DFW_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "houston",
+      "name": "Houston",
+      "state": "TX",
+      "handle": "@cashstashhtx",
+      "instagram": "https://www.instagram.com/cashstashhtx",
+      "form": "https://formspree.io/f/REPLACE_HOUSTON_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "louisiana",
+      "name": "Louisiana",
+      "state": "LA",
+      "handle": "@cashstashlouisiana",
+      "instagram": "https://www.instagram.com/cashstashlouisiana",
+      "form": "https://formspree.io/f/REPLACE_LOUISIANA_FORM",
+      "sponsors": []
+    },
+    {
+      "id": "new-orleans",
+      "name": "New Orleans",
+      "state": "LA",
+      "handle": "@nolacashstash",
+      "instagram": "https://www.instagram.com/nolacashstash",
+      "form": "https://formspree.io/f/REPLACE_NOLA_FORM",
+      "sponsors": []
+    }
+  ]
+};
